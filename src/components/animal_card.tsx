2856806bacb9f18e6157e771f8ff85bc1fa45980
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-10-20 19:41:52 +0100

    feat: Comma separated food items, Extension 2

--- src/components/animal_card.tsx
+++ src/components/animal_card.tsx
@@ -142,7 +142,7 @@ const AnimalCard: React.FC<AnimalCardProps> = ({
       )}
       <h3 className="card__text card__header">{name}</h3>
       <p className="card__text">Species: {species}</p>
-      <p className="card__text">Favourite Food(s): {favFoods}</p>
+      <p className="card__text">Favourite Food(s): {favFoods.join(",")}</p>
       <p className="card__text">Birth Year: {birthYear} </p>
     </div>
   );
